a57b7a638b89875fa43e582f9211cd9eafaae708
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-17 20:03:51 +0200

    work in progress

--- srcs/reverse_rotate.c
+++ srcs/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:55:49 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/17 16:19:35 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/17 19:57:29 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,43 @@
 
 void	reverse_rotate_a(t_stack *stack)
 {
-	int	tmp;
-	int	idx;
+	int	a;
+	int	b;
 
-	tmp = stack->data[stack->size];
-	idx = stack->size - 1;
-	while (idx > stack->idx_a)
+	a = stack->size + 1;
+	b = a - 1;
+	while (b > stack->idx_a)
 	{
-		stack->data[idx + 1] = stack->data[idx];
-		--idx;
+		stack->data[a] ^= stack->data[b];
+		stack->data[b] ^= stack->data[a];
+		stack->data[a] ^= stack->data[b];
+		--a;
+		--b;
 	}
-	stack->data[stack->idx_a] = tmp;
 	ft_printf("rra\n");
 }
 
 void	reverse_rotate_b(t_stack *stack)
 {
-	int	tmp;
-	int	idx;
+	int	a;
+	int	b;
 
-	tmp = stack->data[0];
-	idx = 1;
-	while (idx < stack->idx_a)
+	a = stack->idx_a - 1;
+	b = a - 1;
+	while (b >= 0)
 	{
-		stack->data[idx - 1] = stack->data[idx];
-		++idx;
+		stack->data[a] ^= stack->data[b];
+		stack->data[b] ^= stack->data[a];
+		stack->data[a] ^= stack->data[b];
+		--a;
+		--b;
 	}
-	stack->data[stack->idx_a - 1] = tmp;
 	ft_printf("rrb\n");
 }
 
 void	reverse_rotate_a_b(t_stack *stack)
 {
 	reverse_rotate_a(stack);
-	reverse_rotate_a(stack);
+	reverse_rotate_b(stack);
 	ft_printf("rrr\n");
 }
