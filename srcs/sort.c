30da23a975797c64d937e5ea159ef355cc4124ac
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-20 13:44:24 +0200

    working for every 3 element in a

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:06:39 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/20 12:45:42 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/20 13:43:44 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 void	sort_3(t_stack *s)
 {
-	if (s->data[0] > s->data[1])
+	if (s->data[s->idx_a] > s->data[s->idx_a + 1])
 	{
-		if (s->data[0] > s->data[2])
+		if (s->data[s->idx_a] > s->data[s->idx_a + 2])
 		{
-			if (s->data[2] > s->data[1])
-				rotate_a(s); // 201
+			if (s->data[s->idx_a + 2] > s->data[s->idx_a + 1])
+				rotate_a(s);
 			else
 			{
 				swap_a(s);
-				reverse_rotate_a(s); // 210
+				reverse_rotate_a(s);
 			}
 		}
-		else
-			swap_a(s); // 102
 	}
 	else
 	{
-		if (s->data[1] > s->data[2])
+		if (s->data[s->idx_a + 1] > s->data[s->idx_a + 2])
 		{
-			if (s->data[0] > s->data[2])
-				reverse_rotate_a(s); // 120
+			if (s->data[s->idx_a] > s->data[s->idx_a + 2])
+				reverse_rotate_a(s);
 			else
 			{
 				swap_a(s);
-				rotate_a(s); // 021
+				rotate_a(s);
 			}
 		}
 	}
