2ad4054641ccebbae2fc1600eab75fec6a474e8d
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-21 19:57:31 +0200

    split sort added and now sort_5 is use less

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:06:39 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/20 20:14:51 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/21 19:56:36 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,35 +60,71 @@ void	sort_3(t_stack *s)
 	}
 }
 
-void	sort_5(t_stack *s)
+// void	sort_5(t_stack *s)
+// {
+// 	int	idx;
+// 	idx = s->size;
+// 	while (idx > 3)
+// 	{
+// 		while (s->data[s->idx_a] != 0 && s->data[s->idx_a] != 1)
+// 			rotate_a(s);
+// 		push_b(s);
+// 		--idx;
+// 	}
+// 	sort_3(s);
+// 	if (s->data[s->idx_a - 1] < s->data[s->idx_a - 2])
+// 		swap_b(s);
+// 	while (idx < s->size)
+// 	{
+// 		push_a(s);
+// 		++idx;
+// 	}
+// }
+
+void	split_sort(t_stack *s, int split, int len)
 {
 	int	idx;
-	// int	i;
 
-	idx = s->size;
-	// i = 0;
-	while (idx > 3)
+	idx = 0;
+	while (idx < len + 1)
 	{
-		while (s->data[s->idx_a] != 0 && s->data[s->idx_a] != 1)
+		if (!(s->idx_a < s->size - 3))
+			return ;
+		if (s->data[s->idx_a] >= split)
 			rotate_a(s);
-		push_b(s);
-		// ++i;
-		--idx;
+		else
+		{
+			if (s->idx_a - 1 > 0 && s->data[s->idx_a - 1] <= split / 2)
+				rotate_b(s);
+			push_b(s);
+		}
+		++idx;
+		if (s->data[s->idx_a - 1] < split / 2)
+			rotate_b(s);
+	}
+}
+
+void	sort_100(t_stack *s)
+{
+	int	split;
+	int	loop;
+
+	loop = 3;
+	split = 0;
+	while (s->idx_a < s->size - 3)
+	{
+		split = (((s->size - split) / loop) + split) * 2;
+		split_sort(s, split, s->size - s->idx_a);
 	}
 	sort_3(s);
-	if (s->data[s->idx_a - 1] < s->data[s->idx_a - 2])
-		swap_b(s);
-	while (idx < s->size)
-	{
-		push_a(s);
-		++idx;
-	}
 }
 
 void	sort(t_stack *s)
 {
 	if (s->size <= 3)
 		sort_3(s);
+	// else if (s->size == 5)
+	// 	sort_5(s);
 	else
-		sort_5(s);
+		sort_100(s);
 }
