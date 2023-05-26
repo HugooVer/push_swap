7f587e35f39b602b60f754df31126cdeaf5701c8
HugooVer <hugo.vercelloneg@gmail.com>
2023-05-26 10:07:33 +0200

    back to multiple same line for the norme

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: hvercell <hvercell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:06:39 by hvercell          #+#    #+#             */
-/*   Updated: 2023/05/26 10:01:41 by hvercell         ###   ########.fr       */
+/*   Updated: 2023/05/26 10:06:40 by hvercell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,27 +72,26 @@ void	after_split_sort(t_stack *s)
 			push_a(s);
 		}
 		--tab[2];
-		tab[0] = 0;
 		--tab[1];
+		tab[0] = 0;
 	}
 }
 
 void	split_sort(t_stack *s, int split, int len, double oui)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
 	while (i < len + 1)
 	{
 		if (!(s->idx_a < s->size - 3))
 			return ;
-		while (j < 3)
-		{
-			if (s->data[s->idx_a] > s->size - 4)
-				rotate_a(s);
-		}
+		if (s->data[s->idx_a] > s->size - 4)
+			rotate_a(s);
+		if (s->data[s->idx_a] > s->size - 4)
+			rotate_a(s);
+		if (s->data[s->idx_a] > s->size - 4)
+			rotate_a(s);
 		if (s->data[s->idx_a] > split)
 			rotate_a(s);
 		else
